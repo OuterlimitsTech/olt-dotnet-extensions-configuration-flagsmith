cb6cc7a8279e508ab4ddf65914d27e49dc6e057d
Chris Straw <chris.straw@outerlimitstech.com>
2021-11-30 07:09:44 -0500

    wip

--- tests/OLT.Extensions.Configuration.Flagsmith.Tests/BaseTests.cs
+++ tests/OLT.Extensions.Configuration.Flagsmith.Tests/BaseTests.cs
@@ -3,7 +3,7 @@
     public abstract class BaseTests
     {
 #pragma warning disable CS8603 // Possible null reference return.
-        public static string ApiKey => System.Environment.GetEnvironmentVariable("FLAGSMITH_API_KEY");
+        public string ApiKey => System.Environment.GetEnvironmentVariable("FLAGSMITH_API_KEY");
 #pragma warning restore CS8603 // Possible null reference return.
 
     }
